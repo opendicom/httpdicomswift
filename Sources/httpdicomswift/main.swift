2e661a16eee1acec06009a5384b4488b1e30b5ea
Claudio Baeza <cl.baeza@gmail.com>
2019-04-30 19:40:25 -0400

    main K setup

--- Sources/httpdicomswift/main.swift
+++ Sources/httpdicomswift/main.swift
@@ -1,13 +1,54 @@
-let server = Server()
+import Foundation
+/*let server = Server()
+server.listen(11114)*/
 
-server.listen(11114)
+let deployPath = "/Users/Shared/GitHub/httpdicom/deploy/"
+let fileManager = FileManager.default
 
+let schema = NSDictionary(contentsOfFile: deployPath + "voc/scheme.plist") as! Dictionary<String,AnyObject>
+let schemaIndexes = Dictionary.dictionaryOfArray(from: schema)
 
+var code = Dictionary<String, AnyObject>()
+var codeIndexes = Dictionary<String, [AnyObject]>()
+let codeFiles = try fileManager.contentsOfDirectory(atPath: deployPath +  "voc/code/")
+for codeFile in codeFiles {
+    if codeFile.hasPrefix("."){
+        continue
+    }else{
+        let file = NSDictionary(contentsOfFile: deployPath + "voc/code/" + codeFile) as! Dictionary<String,AnyObject>
+        code.updateValue(file as AnyObject, forKey: codeFile.replacingOccurrences(of: ".plist", with: ""))
+        codeIndexes.updateValue([Dictionary.dictionaryOfArray(from: file) as AnyObject], forKey: codeFile.replacingOccurrences(of: ".plist", with: ""))
+    }
+}
 
+var procedure = Dictionary<String, AnyObject>()
+var procedureIndexes = Dictionary<String, [AnyObject]>()
+let procedureFiles = try fileManager.contentsOfDirectory(atPath: deployPath + "voc/procedure/")
+for procedureFile in procedureFiles {
+    if procedureFile.hasPrefix("."){
+        continue
+    }else{
+        let file = NSDictionary(contentsOfFile: deployPath + "voc/procedure/" + procedureFile) as! Dictionary<String,AnyObject>
+        procedure.updateValue(file as AnyObject, forKey: procedureFile.replacingOccurrences(of: ".plist", with: ""))
+        procedureIndexes.updateValue([Dictionary.dictionaryOfArray(from: file) as AnyObject], forKey: procedureFile.replacingOccurrences(of: ".plist", with: ""))
+    }
+}
+//Default time zone "-0400"
+K.setup(defaultTimezone: "-0400",
+        scheme: schema,
+        schemeIndexes: schemaIndexes!,
+        code: code,
+        codeIndexes: codeIndexes,
+        procedure: procedure,
+        procedureIndexes: procedureIndexes)
 
-
-
-
+print(K.shared.defaultTimezone)
+print(K.shared.scheme)
+print(K.shared.schemeIndexes)
+print(K.shared.code)
+print(K.shared.codeIndexes)
+print(K.shared.procedure)
+print(K.shared.procedureIndexes)
 
 //===----------------------------------------------------------------------===//
 //
